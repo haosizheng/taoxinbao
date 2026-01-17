fb09cd97d51df4dc9ea3d229d8c85fa5fffd294a
haosizheng <sizheng1102@gmail.com>
2026-01-17 11:48:48 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# hide side menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+st.title("Hello World!")
